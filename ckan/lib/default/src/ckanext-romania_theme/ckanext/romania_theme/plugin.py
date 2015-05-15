fd783455dc06cef01ba7edb202f476831621cea3
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-05-15 14:07:00 +0300

    Add airbrake

--- ckan/lib/default/src/ckanext-romania_theme/ckanext/romania_theme/plugin.py
+++ ckan/lib/default/src/ckanext-romania_theme/ckanext/romania_theme/plugin.py
@@ -1,5 +1,10 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+import airbrake
+
+# Setup logger
+# TODO @palcu: remove these once the project goes public
+logger = airbrake.getLogger(api_key='d6652cdfa031e900485e34539212531d', project_id=111438)
 
 
 class Romania_ThemePlugin(plugins.SingletonPlugin):
