f97eceab2c4dad047951fa1476615725875f2c40
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-05-21 14:56:46 +0300

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 . /usr/lib/ckan/default/bin/activate
 rm -rf /usr/lib/ckan/default/src/ckanext-romania_theme/
-cp -R ckan/lib/src/ckanext-romania_theme/ /usr/lib/ckan/default/src/
-python setup.py develop /usr/lib/ckan/default/src/ckanext-romania_theme/setup.py
+cp -R ckan/lib/default/src/ckanext-romania_theme/ /usr/lib/ckan/default/src/
+python /usr/lib/ckan/default/src/ckanext-romania_theme/setup.py develop
