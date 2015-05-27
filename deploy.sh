6c3ce72b73f2ba0a59e38be62943e2b8075efa24
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-05-27 16:39:45 +0300

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-. /usr/lib/ckan/default/bin/activate
-rm -rf /usr/lib/ckan/default/src/ckanext-romania_theme/
-cp -R ckan/lib/default/src/ckanext-romania_theme/ /usr/lib/ckan/default/src/
-python /usr/lib/ckan/default/src/ckanext-romania_theme/setup.py develop
