437ff5a789d9e7678ea45856a52140e3103052ee
Alex Palcuie <alex.palcuie@gmail.com>
2016-06-15 18:12:31 +0300

    Pin last working Ansible version

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -2,4 +2,4 @@
 
 apt-add-repository ppa:ansible/ansible -y
 apt-get update -qq
-apt-get install git ansible software-properties-common -qq
+apt-get install git ansible=2.0.2.0-1ppa~trusty software-properties-common -qq
