f58c2cf8cc2e7ff1393e18083248746e354059ad
Alex Palcuie <alex.palcuie@gmail.com>
2016-06-29 18:27:03 +0300

    Update install-dependencies.sh

--- provisioning/install-dependencies.sh
+++ provisioning/install-dependencies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 apt-get update -qq
-apt-get install git software-properties-common python-pip -qq
+apt-get install git software-properties-common python-dev python-pip -qq
 
 pip install ansible==2.0.2.0
