898682aa00e54095d91eb27ff9657220d2ee5e52
Popa Razvan <raztechs@users.noreply.github.com>
2016-07-04 13:53:01 +0300

    Update install-dependencies.sh

--- provisioning/install-dependencies.sh
+++ provisioning/install-dependencies.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 apt-get update -qq
-apt-get install git software-properties-common python-dev python-pip -qq
+apt-get install git software-properties-common python-dev python-pip build-essential libssl-dev libffi-dev -qq
 
+pip install markupsafe
 pip install ansible==2.0.2.0
