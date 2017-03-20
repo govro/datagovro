99d8751ddc0cf99cabb0951c5e7abaf598910ebd
Popa Razvan <razz.popa@gmail.com>
2017-03-20 12:55:41 +0200

    Fix installer dependencies

--- provisioning/install-dependencies.sh
+++ provisioning/install-dependencies.sh
@@ -3,5 +3,9 @@
 apt-get update -qq
 apt-get install git software-properties-common python-dev python-pip build-essential libssl-dev libffi-dev -qq
 
+apt-get install postgresql-9.3 -qq
+pg_createcluster 9.3 main --start
+
 pip install markupsafe
 pip install ansible==2.0.2.0
+pip install setuptools==11.3
